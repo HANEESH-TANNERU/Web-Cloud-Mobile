3b46da9de4bff9c9519d74f797fdc964c30b9c95
Sai Haneesh Tanneru <54335519+HANEESH-TANNERU@users.noreply.github.com>
2020-06-26 23:49:55 -0500

    Add files via upload

---
+++ ICP6/MashUp/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  FoursquareAPICLIENTID : "KMG5XDOWTX4FMSLSEUTGOTPPZF5XN5KUY0NMXOU2ATXX540L",
+  FoursquareAPICLIENTKEY  : "P024YEOO2OLBDERBFJWL2OKB2QHYY3FYXCDXQFYJHIR21CD0",
+  EDAMAMAPPID : "4c323871",
+  EDADAMAPPKEY : "fb2a3c759f69e41b449a034266667fe2",
+
+
+
+
+
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
